513d022cbe1c1156e1c863081b96ca5dea104380
BablVII <BablVII@yandex.ru>
2018-11-02 11:41:06 +0700

    Class 'app\controllers\Yii' not found #1

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -3,11 +3,27 @@
 namespace app\controllers;
 
 use yii\web\Controller;
+use app\models\EntryForm;
+use yii;
 
 class SiteController extends Controller {
 
     public function actionIndex() {
         return "Hello World!";
+    }
+
+    public function actionEntry() {
+        $model = new EntryForm();
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // данные в $model удачно проверены
+            // делаем что-то полезное с $model ...
+
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // либо страница отображается первый раз, либо есть ошибка в данных
+            return $this->render('entry', ['model' => $model]);
+        }
     }
 
 }
